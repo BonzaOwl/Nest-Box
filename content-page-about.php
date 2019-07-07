6a6142b22e54ee92d4d4ff8d1e2c85280dd40cdc
Bonza Owl <bonzaowl@gmail.com>
2019-07-07 11:32:11 +0100

    Created a page template for the about page

---
+++ content-page-about.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The template for displaying the page content
+ *
+ *
+ */?>
+
+<div class="container">
+    <h1><?php the_title(); ?></h1>
+    <div class="timeline">
+        <div class="timeline-item active">
+            <div class="year">2008 <span class="marker"><span class="dot"></span></span>
+            </div>
+            <div class="info">Test Test Test Test Test.</div>
+        </div>
+        <div class="timeline-item">
+            <div class="year">2008 <span class="marker"><span class="dot"></span></span>
+            </div>
+            <div class="info">Test Test Test Test Test.</div>
+        </div>
+        <div class="timeline-item">
+            <div class="year">2008 <span class="marker"><span class="dot"></span></span>
+            </div>
+            <div class="info">Test Test Test Test Test.</div>
+        </div>
+        <div class="timeline-item">
+            <div class="year">2008 <span class="marker"><span class="dot"></span></span>
+            </div>
+            <div class="info">Test Test Test Test Test.</div>
+        </div>
+        <div class="timeline-item">
+            <div class="year">2008 <span class="marker"><span class="dot"></span></span>
+            </div>
+            <div class="info">Test Test Test Test Test.</div>
+        </div>
+        <div class="timeline-item">
+            <div class="year">2008 <span class="marker"><span class="dot"></span></span>
+            </div>
+            <div class="info">Test Test Test Test Test.</div>
+        </div>    
+    </div>
+</div>
---
+++ page-about.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: AboutPage
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ */
+
+get_header(); 
+    if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+		get_template_part( 'content-page-about', get_post_format() );
+    endwhile; endif; 
