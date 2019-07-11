89a834359c4ea136e79a671cb756899bac6eedd2
Bonza Owl <bonzaowl@gmail.com>
2019-07-11 22:05:01 +0100

    Removed old navigation bar in favour of new one.

--- header.php
+++ header.php
@@ -10,30 +10,18 @@
   <head>
     <!-- Call the wordpress header to get meta tags and enqueue styles -->
     <?php wp_head(); ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
   </head>
 
   <body>
 
-  <!-- The main site navigation bar, this is responsive -->
-  <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-  <img src="<?php echo get_bloginfo('template_directory'); ?>/images/logo.png" width="30px" height="30px" /> 
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbar">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-          <a class="nav-link" href="<?php echo site_url(); ?>">Home</a>
-          </li>
-          <li class="nav-item">
-                <a class="nav-link" href="<?php echo site_url(); ?>/about">About</a>
-            </li>  
-        </ul>
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="https://github.com/BonzaOwl" target="_blank"><i class="fab fa-github"></i></a>
-            </li>            
-        </ul>
+  <nav class="nav">
+    <div class="nav-container">
+      <div class="links">
+      <a class="link" style="padding-left:10px; padding-right: 10px;font-weight:bold;font-size:18px;text-decoration:none;color: #ffd17e" href="<?php echo site_url(); ?>">Home</a>
+      <a class="link" style="padding-left:10px; padding-right: 10px;font-weight:bold;font-size:18px;text-decoration:none;color: #ffd17e" href="<?php echo site_url(); ?>/about">Me</a>
+      <a class="link" style="padding-left:10px; padding-right: 10px;font-weight:bold;font-size:18px;text-decoration:none;color: #ffd17e" href="https://github.com/BonzaOwl" target="_blank"><i class="fab fa-github"></i></a>
+      <a class="link" style="padding-left:10px; padding-right: 10px;font-weight:bold;font-size:18px;text-decoration:none;color: #ffd17e" href="https://twitter.com/BonzaOwl" target="_blank"><i class="fab fa-twitter"></i></a>
       </div>
-    </nav>
+    </div>
+  </nav>
