83a0e7e7fa0d3907e7c3044b7d1107784fa754ca
Bonza Owl <bonzaowl@gmail.com>
2019-07-29 08:23:49 +0100

    Tidied up the main content template to allow for better displaying of the post information.

--- content.php
+++ content.php
@@ -5,14 +5,23 @@
  */
 ?>
 
-<a class="post" id="post-<?php the_ID(); ?>" href="<?php the_permalink(); ?>">
-<span class="single-category mobile-hide">
-<?php
-foreach((get_the_category()) as $category) { 
-    echo $category->cat_name . ' '; 
-} 
-?>
-</span>
-  <div class="post-title"><?php the_title(); ?></div>
-	  <span class="post-date"><time><?php the_time( 'F j, Y' ); ?></time></span>
-</a>
+<div class="home-post" style="background: #ffd17e;padding:.75rem;margin-top: .75rem;margin-bottom: .75rem;margin-left: .75rem;margin-right: .75rem;border-radius:4px;"> 
+
+
+<span class="home-post-date" style="display:inline">
+    <time>Posted: <?php the_time( 'F j, Y' ); ?></time>
+  </span>
+
+<span class="single-category mobile-hide" style="display:inline"> 
+    <?php
+    foreach((get_the_category()) as $category) { 
+        ?> <span class="category-high"> <?php echo $category->cat_name . ' ' ?> </span> <?php; 
+    } 
+    ?>
+  </span>
+  
+  <a id="post-<?php the_ID(); ?>" href="<?php the_permalink(); ?>">
+    <div class="home-post-title"><?php the_title(); ?></div>
+  </a>
+
+</div>
